1088b44bb2dd08337b0e20b13113dda6b73a2ea3
mduong1701 <minhmanlk0411@gmail.com>
2022-08-29 16:45:32 -0700

    Added Sensei class

--- 08.29/ninja.js
+++ 08.29/ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
-    constructor(name, health){
+    constructor(name, health, speed, strength){
         this.name = name;
         this.health = health;
-        this.speed = 3;
-        this.strength = 3;
+        this.speed = speed;
+        this.strength = strength;
     }
 
     sayName(){
@@ -18,3 +18,19 @@ class Ninja {
         this.health += 10;
     }
 }
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name, 200, 10, 10);
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
