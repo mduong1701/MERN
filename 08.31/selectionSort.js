989872542018e2a8b78d1cc8e09f963984ac1ba4
Man Duong <mduong8911@hotmail.com>
2022-08-31 13:59:22 -0700

    Finished prop_it_up

--- 08.31/selectionSort.js
+++ 08.31/selectionSort.js
@@ -1,5 +1,5 @@
 function selectionSort(arr){
-    for(let i = 0; i < arr.length - 1; i++){
+    for(let i = 0; i < arr.length; i++){
         let minIndex = i;
         for (let j = i + 1; j < arr.length; j++){
             if (arr[minIndex] > arr[j]){
@@ -19,3 +19,4 @@ console.log(selectionSort([6,5,4,3,2,1]));
 console.log(selectionSort([6,2,4,1,5,3]));
 console.log(selectionSort([5,3,6,2,4,1]));
 console.log(selectionSort([34,78,21,27,38,22]));
+console.log(selectionSort([]));
--- PROJECTS/prop_it_up/src/components/Person.jsx
+++ PROJECTS/prop_it_up/src/components/Person.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 
-class Person extends React.Component{
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //         p_age: this.props.age
-    //     };
-    // }
+class Person extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            p_age: this.props.age
+        };
+    }
 
-    increaseAge = () => { this.setState((prevState) => { return { p_age: prevState.p_age + 1}})}
-    
+    increaseAge = () => {
+        console.log("Hello");
+        this.setState({ p_age: this.state.p_age + 1 });
+        // this.setState((state) => {
+        //     return { p_age: state.p_age + 1 };
+        // });
+    }
+
     render() {
         return (
             <fieldset>
                 <h2>{this.props.last_name}, {this.props.first_name}</h2>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.p_age}</p>
                 <p>Hair color: {this.props.hair_color}</p>
-                <button onClick={ this.increaseAge }>Birthday Button for {this.props.first_name} {this.props.last_name}</button>
+                <button onClick={this.increaseAge}>Birthday Button for {this.props.first_name} {this.props.last_name}</button>
             </fieldset>
         );
     }
