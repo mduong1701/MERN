7c3611c0b7f454ae0a0289f45970fae44d023bff
mduong1701 <minhmanlk0411@gmail.com>
2022-09-03 14:41:38 -0700

    Fixed a bug

--- PROJECTS/to_do_list/src/components/AddItem.jsx
+++ PROJECTS/to_do_list/src/components/AddItem.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from 'react'
 
 const AddItem = (props) => {
-    const [ input, setInput ] = useState("")
-    props.addItem(input)
+    const [input, setInput] = useState("")
+    
+    const submitHandler = (event) => {
+        event.preventDefault();
+        props.addItem(input);
+        event.target.reset();
+    }
     return (
         <fieldset>
             <legend>Add a new task</legend>
-            <input onChange={
-                (e) => {
-                    setInput(e.target.value)
-                }
-            }/>
-            <button
-                style={{
-                    display:'block',
-                    margin: '0 auto',
-                    height: '30px',
-                    width: '80px',
-                    backgroundColor: 'green',
-                    borderRadius: '5px'
-                }}>
-                Add
-            </button>
+            <form onSubmit = {submitHandler}>
+                <input onChange={
+                    (e) => {
+                        setInput(e.target.value)
+                    }
+                } />
+                <button
+                    style={{
+                        display: 'block',
+                        margin: '0 auto',
+                        height: '30px',
+                        width: '80px',
+                        backgroundColor: 'green',
+                        borderRadius: '5px'
+                    }}>
+                    Add
+                </button>
+            </form>
         </fieldset>
     )
 }
