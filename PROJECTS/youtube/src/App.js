7bc5ba504ebbcb65c3ed319845ca7d93a10afd01
mduong1701 <minhmanlk0411@gmail.com>
2022-09-02 18:08:38 -0700

    Finished Youtube project

--- PROJECTS/youtube/src/App.js
+++ PROJECTS/youtube/src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState} from 'react'
+import AddTask from './components/AddTask'
+import { useState } from 'react'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -23,17 +25,41 @@ function App() {
         reminder: false,
     },
 ])
-
-  //Delete Task
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
+  // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => 
+    task.id === id
+    ? {...task, reminder: !task.reminder}
+    : task
+    ));
+  }
+
   return (
     <div className="container">
-      <Header />
+      <Header 
+      onAdd={
+        () => setShowAddTask(!showAddTask)
+      }
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 
-      ? <Tasks tasks = {tasks} onDelete={deleteTask}/>
+      ? <Tasks 
+      tasks = {tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder}
+      />
       : 'No Tasks to show'}
     </div>
   );
--- PROJECTS/youtube/src/components/Header.jsx
+++ PROJECTS/youtube/src/components/Header.jsx
@@ -1,15 +1,25 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+const Header = ({title, onAdd, showAdd}) => {
+    
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button text='Add' onClick={onClick}/>
+            <Button 
+            text={
+                showAdd 
+                ? 'Close'
+                : 'Add'
+            }
+            onClick={onAdd}
+            color={
+                showAdd 
+                ? 'red'
+                : 'green'
+            }
+            />
         </header>
     )
 }
