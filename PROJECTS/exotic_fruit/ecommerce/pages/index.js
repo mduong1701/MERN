62584082bc5f9303ae04d7cb55e868c2425f2488
mduong1701 <minhmanlk0411@gmail.com>
2022-09-22 20:30:23 -0700

    Fixed bugs

---
+++ PROJECTS/exotic_fruit/ecommerce/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = () => {
+  return (
+    <>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className="products-container">
+        {['Product 1', 'Product 2'].map( (product) => product)}
+      </div>
+      <FooterBanner />
+    </>
+  )
+}
+
+export default Home
